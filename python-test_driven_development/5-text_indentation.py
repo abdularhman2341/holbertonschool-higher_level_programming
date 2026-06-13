295e374bd0009d50771e2e12e6d85b5e7617e40b
abdularham2341 <abdulrhman3334@gmail.com>
2026-06-13 03:24:29 +0300

    Fix pycodestyle trailing whitespace and blank lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,21 +9,17 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    i = 0
-    text_len = len(text)
-
-    
-    while i < text_len and text[i] == ' ':
-        i += 1
-
-    while i < text_len:
-        print(text[i], end="")
-        if text[i] == "\n" or text[i] in ".?:":
-            if text[i] in ".?:":
-                print("\n")
-            i += 1
-            
-            while i < text_len and text[i] == ' ':
-                i += 1
-            continue
-        i += 1
+    flag = 0
+    for char in text:
+        if flag == 0:
+            if char == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if char in "?.:":
+                print(char)
+                print("")
+                flag = 0
+            else:
+                print(char, end="")
