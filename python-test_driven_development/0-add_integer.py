6223f865103cd1cc727be1825beeced501f7eaa8
abdularham2341 <abdulrhman3334@gmail.com>
2026-06-13 03:09:13 +0300

    Fix pycodestyle and edge cases for all tasks

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,11 +3,11 @@
 Module for addition of two integers.
 """
 
+
 def add_integer(a, b=98):
-    """Adds two integers after checking their types and casting floats to ints."""
+    """Adds two integers."""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-Module to print a square using the character #.
+Module to indent text based on specific punctuation.
 """
 
-def print_square(size):
-    """Prints a square with the character # of a given size."""
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-        
-    for i in range(size):
-        print("#" * size)
+
+def text_indentation(text):
+    """Prints text with 2 new lines after ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    skip = True
+    for char in text:
+        if skip and char == ' ':
+            continue
+        skip = False
+
+        if char in ".?:":
+            print(char)
+            print("")
+            skip = True
+        else:
+            print(char, end="")
