36642cd5beaa52f110d7cc84dcc9736b698ab62e
abdularham2341 <abdulrhman3334@gmail.com>
2026-06-25 13:59:47 +0300

    Fix PEP8 line length issue in module docstring

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines the Square class with a specific string representation."""
+"""Module that defines the Square class."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
