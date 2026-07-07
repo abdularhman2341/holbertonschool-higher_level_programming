d33b4c29d4d1b8042bb0f67ed233471194a482cc
abdularham2341 <abdulrhman3334@gmail.com>
2026-07-07 11:00:15 +0300

    feat: implement fetch_and_save_posts to export data to CSV

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,18 +1,32 @@
 import requests
+import csv
 
 def fetch_and_print_posts():
-    
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     
-    
     print(f"Status Code: {response.status_code}")
     
-
     if response.status_code == 200:
-        
         posts = response.json()
-        
-        
         for post in posts:
             print(post['title'])
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        structured_data = []
+        
+        for post in posts:
+            post_dict = {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            structured_data.append(post_dict)
+            
+        with open('posts.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            writer.writerows(structured_data)
