70630b86d20fac94d7829e7bc5a57dd4b263486a
abdularham2341 <abdulrhman3334@gmail.com>
2026-07-25 17:00:40 +0300

    Fix case-sensitive state name filtering

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = connection.cursor()
     query = (
         "SELECT * FROM states "
-        "WHERE name = '{}' "
+        "WHERE name = BINARY '{}' "
         "ORDER BY id ASC"
     ).format(sys.argv[4])
     cursor.execute(query)
