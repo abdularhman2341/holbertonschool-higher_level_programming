368b6084fe488ad0b23205bff5a58d86c2f7c113
abdularham2341 <abdulrhman3334@gmail.com>
2026-07-25 17:11:18 +0300

    Add script to list cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Lists all cities with their corresponding states."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "INNER JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+
+    for city in cursor.fetchall():
+        print(city)
+
+    cursor.close()
+    connection.close()
