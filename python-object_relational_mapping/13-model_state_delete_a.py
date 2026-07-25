0688f2e8f456c66330e19fe9a401adef1a6f426e
abdularham2341 <abdulrhman3334@gmail.com>
2026-07-25 17:41:53 +0300

    Add script to delete states containing a

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Deletes all states whose names contain the letter a."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    states = session.query(State).filter(
+        State.name.contains("a")
+    ).all()
+
+    for state in states:
+        session.delete(state)
+
+    session.commit()
+    session.close()
