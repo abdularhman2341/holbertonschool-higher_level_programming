6aea6a3eeac716517deff227854c2367ecf537e0
abdularham2341 <abdulrhman3334@gmail.com>
2026-08-08 14:58:25 +0300

    Print a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  let row = '';
+
+  for (let i = 0; i < size; i++) {
+    row += 'X';
+  }
+
+  for (let i = 0; i < size; i++) {
+    console.log(row);
+  }
+}
